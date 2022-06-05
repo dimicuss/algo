6ebccfc8daeb9d518267aa60d9f25d9c9592131d
dimicuss <dimicuss@gmail.com>
2022-06-05 10:55:30 +0300

    Remove some files

--- app/components/NewSlotMachine/lib/createCircleListArray.js
+++
@@ -1,9 +0,0 @@
-import { includes } from 'lodash';
-
-export default function createCircleListArray(list, array = []) {
-  if (includes(array, list) || list === null) {
-    return array;
-  }
-  array.push(list);
-  return createCircleListArray(list.next, array);
-}
--- app/components/NewSlotMachine/lib/getSpinCount.js
+++
@@ -1,5 +0,0 @@
-export default function getSpinCount(currentItem, key, index, result = 0) {
-  return currentItem.index === index
-    ? result
-    : getSpinCount(currentItem[key], key, index, result + 1);
-}
